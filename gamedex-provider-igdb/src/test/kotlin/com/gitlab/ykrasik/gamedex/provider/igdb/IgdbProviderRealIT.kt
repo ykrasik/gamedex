4182e0125ab4df1d6e3e2ec4860ea4e8270de5e5
ykrasik <ykrasik@gmail.com>
2017-05-20 20:10:43 +0300

    Upgrade Exposed to 0.9

--- gamedex-provider-igdb/src/test/kotlin/com/gitlab/ykrasik/gamedex/provider/igdb/IgdbProviderRealIT.kt
+++ gamedex-provider-igdb/src/test/kotlin/com/gitlab/ykrasik/gamedex/provider/igdb/IgdbProviderRealIT.kt
@@ -55,12 +55,12 @@ class IgdbProviderRealIT : ScopedWordSpec() {
     val apiUrl = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/3225"
     val releaseDate = "2016-08-12"
     val criticScore = 72.6428571428571
-    val userScore = 63.0478057301822
+    val userScore = 61.25741248922451
     val thumbnailUrl = "http://images.igdb.com/igdb/image/upload/t_thumb_2x/sixpdbypwojsyly22a1l.png"
     val posterUrl = "http://images.igdb.com/igdb/image/upload/t_screenshot_huge/sixpdbypwojsyly22a1l.png"
     val url = "https://www.igdb.com/games/no-man-s-sky"
     val description = "Inspired by the adventure and imagination that we love from classic science-fiction, No Man's Sky presents you with a galaxy to explore, filled with unique planets and lifeforms, and constant danger and action. \n\nIn No Man's Sky, every star is the light of a distant sun, each orbited by planets filled with life, and you can go to any of them you choose. Fly smoothly from deep space to planetary surfaces, with no loading screens, and no limits. In this infinite procedurally generated universe, you'll discover places and creatures that no other players have seen before - and perhaps never will again."
-    val genres = listOf("Shooter", "Role-playing (RPG)", "Simulator", "Adventure", "Indie")
+    val genres = listOf("Shooter", "Role-Playing Game (RPG)", "Simulation", "Adventure", "Indie")
     val screenshotUrls = listOf(
         "http://images.igdb.com/igdb/image/upload/t_screenshot_huge/slbfwlrvqjrgvrvfghrk.png",
         "http://images.igdb.com/igdb/image/upload/t_screenshot_huge/xufkotfn5udk2aijb6f0.png",
