5ffb169caff288c723e2af2f02102708f9bf2018
ykrasik <ykrasik@gmail.com>
2019-02-09 14:33:14 +0200

    Add a Version class that is generated by Gradle according to the latest Git tag.

---
+++ core/gamedex-core-common/src/main/kotlin/com/gitlab/ykrasik/gamedex/core/version/Version.kt
@@ -0,0 +1,32 @@
+/****************************************************************************
+ * Copyright (C) 2016-2019 Yevgeny Krasik                                   *
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ * http://www.apache.org/licenses/LICENSE-2.0                               *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ ****************************************************************************/
+
+package com.gitlab.ykrasik.gamedex.core.version
+
+import com.gitlab.ykrasik.gamedex.util.toDateTime
+import net.swiftzer.semver.SemVer
+
+/**
+ * User: ykrasik
+ * Date: 09/02/2019
+ * Time: 14:18
+ */
+object Version {
+    val version = SemVer.parse(VERSION)
+    val commit = GIT_SHA
+    val commitDate = GIT_DATE.toDateTime()
+    val buildDate = BUILD_DATE.toDateTime()
+}
